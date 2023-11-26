911e81df01c0051cea1310bef29cf75076c3fdc2
Rajib Sarker <redhat0593@gmail.com>
2023-11-27 01:46:51 +0600

    all reduced to 10%

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,32 +8,4 @@ var swiper = new Swiper(".mySwiper", {
     },
     loop: true,
     speed: 2000,
-
-    // effect: 'coverflow',
-    // coverflowEffect: {
-    //     rotate: 30,
-    //     slideShadows: false,
-    // },
-
-    // effect: 'creative',
-    // creativeEffect: {
-    //     prev: {
-    //         // will set `translateZ(-400px)` on previous slides
-    //         translate: [0, 0, -400],
-    //     },
-    //     next: {
-    //         // will set `translateX(100%)` on next slides
-    //         translate: ['100%', 0, 0],
-    //     },
-    // },
-
-    effect: 'cube',
-    cubeEffect: {
-        slideShadows: false,
-    },
-
-    // effect: 'fade',
-    // fadeEffect: {
-    //     crossFade: true
-    // }
 });
